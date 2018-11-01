b92b6a98ec3025b3e06774e9a28165dd9391c547
EvanRStewart <43525187+EvanRStewart@users.noreply.github.com>
2018-10-31 22:19:46 -0400

    return transactions from route

--- api/routes/transaction.route.js
+++ api/routes/transaction.route.js
@@ -11,16 +11,13 @@ let Item = require('../models/item');
 Transaction.hasMany(Transaction_Item, {foreignKey: 'transaction_id'});
 Transaction_Item.belongsTo(Transaction, {foreignKey: 'transaction_id'});
 
-Transaction_Item.hasOne(Item, {foreignKey: 'item_id'});
-Item.belongsTo(Transaction_Item, {foreignKey: 'item_id'});
+Transaction_Item.belongsTo(Item, {foreignKey: 'item_id'});
+Item.hasOne(Transaction_Item, {foreignKey: 'item_id'});
 
 // Defined store route
 transactionRoute.route('/member').get(auth.member, function (req, res) {
-    Transaction.findAll({where: {'member_id': req.member_id, 'transaction_paid': 0}, include: [{model: Transaction_Item, include:[Item]}]}).then(transactionsData =>{
-        let transactions = array();
-        
-
-        console.log(transactions)
+    Transaction.findAll({where: {'member_id': req.member_id, 'transaction_paid': 0}, include: [{model: Transaction_Item, include: [Item]}]}).then(transactionsData =>{
+        res.json(transactionsData);
     });
 });
 
